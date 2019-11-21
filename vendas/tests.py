fedced28f58841b8ddbfa78c143c68a51f8cd939
Leandro Casanova <casanova.leandro@gmail.com>
2019-11-21 19:35:15 -0300

    Fix test config

--- vendas/tests.py
+++ vendas/tests.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase, force_authenticate
 
 from .models import Produto
 
@@ -10,18 +10,17 @@ from .models import Produto
 class ProdutoTestCase(APITestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user("tester", "teste@gmail.com", "123456")
+        self.user = User.objects.create_superuser("tester", "teste@gmail.com", "123456")
+        self.client.login(username="tester", password="123456")
+        self.client.force_authenticate(user=self.user)
 
     def test_inclusao_produto(self):
         url = reverse('produto-list')
         data = {'nome': 'Teste Teste',
                 'preco': 123.45,
                 'comissao': 5.55}
-        client = APIClient()
-        client.login(username="tester", password="123456")
-        print(self.user.username)
-        response = client.post(url, data, format='json')
+        response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Produto.objects.count(), 1)
-        self.assertEqual(Produto.objects.get().name, 'Teste Teste')
+        self.assertEqual(Produto.objects.get().nome, 'Teste Teste')
